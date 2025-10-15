b37601539ae574a823ee16630b1418413c655c17
Renan Momesso <renan_momesso@hotmail.com>
2025-10-15 13:46:25 -0300

    Refactor project structure to TypeScript, add type definitions, and remove unused files

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,41 @@
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.bmp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.tiff' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
